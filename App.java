181e19d745b7d884c4d886ee76d229f5315fb671
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-19 09:47:28 +0900

    add sphere instance

--- App.java
+++ App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         Cylinder cylinder = new Cylinder(2, 10);
         System.out.println(cylinder.getVolume());
+
+        Sphere shpere = new Sphere(4);
+        System.out.println(shpere.getVolume());
     }
 }
